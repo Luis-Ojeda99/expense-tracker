4323ce98fc6f4e1237c0adbe9ca63a610d75deb7
Luis Ojeda <luisojeda27180@gmail.com>
2025-10-06 12:38:31 -0600

    Connect backend to PosgreSQL database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
+
 const app = require("./app");
+const pool = require('./config/db')
+
 
 const PORT = process.env.PORT || 5000;
 
 let server;
 
+pool.query('SELECT NOW()', (err, res) => {
+    if(err) {
+        console.error('Database connection error:', err);
+    } else {
+        console.log('Database connected successfully');
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Sever is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
