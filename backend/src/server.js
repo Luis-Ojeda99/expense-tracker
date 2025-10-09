d8f91a60fa88ef2065917560cf104810e0f5fbb7
Luis Ojeda <luisojeda27180@gmail.com>
2025-10-08 21:58:04 -0600

    running server message updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,5 +16,5 @@ pool.query('SELECT NOW()', (err, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running succesfully on port ${PORT}`);
 })
